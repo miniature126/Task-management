80b327964fd6a1c93f47989f0e1b6be4a0380013
R.Nakamura <krw.psh2196@gmail.com>
2020-05-18 09:43:15 +0000

    タスク新規作成ページの機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,17 @@ class TasksController < ApplicationController
   end
 
   def new
-    
+    @task = Task.new
+  end
+
+  def create
+    @task = @user.tasks.new(task_params)
+    if @task.save
+      flash[:success] = "タスクを新規作成しました。"
+      redirect_to root_url
+    else
+      render :new
+    end
   end
 
   def show
@@ -16,5 +26,10 @@ class TasksController < ApplicationController
   private
     def set_user
       @user = User.find_by(params[:user_id])
+    end
+    
+    # strong parameter
+    def task_params
+      params.require(:task).permit(:name, :content, :user_id)
     end
 end

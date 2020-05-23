ec1331403ed89a7a724a28fc45c1ca6ab4591af0
R.Nakamura <krw.psh2196@gmail.com>
2020-05-23 02:28:57 +0000

    タスク新規作成時のエラーメッセージを追加中

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
+  
+  validates :name, presence: true, length: { maximum: 30}
+  validates :content, presence: true, length: { maximum: 300 }
 end

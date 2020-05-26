a3fd1c1843f49d1d420ea36ce7538c90fd3303c0
R.Nakamura <krw.psh2196@gmail.com>
2020-05-26 11:14:05 +0000

    他のユーザー情報を編集できないようにする機能を追加(if編集× users/:idにredirect+flash)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   
   def show
     @user = User.find(params[:id])
@@ -42,10 +43,20 @@ class UsersController < ApplicationController
     end
     
     #beforeフィルター
+    #ログイン済ユーザーか確認
     def logged_in_user
       unless logged_in?
        flash[:danger] = "ログインしてください。"
        redirect_to login_url
+      end
+    end
+    
+    #アクセスユーザーが現在ログインしているユーザーか確認
+    def correct_user
+      @user = User.find(params[:id])
+      unless @user == current_user
+        flash[:danger] = "このユーザーは編集できません。"
+        redirect_to user_url(@user)
       end
     end
 end

16879556ed7711a28ac23545c22585a7fca51487
R.Nakamura <krw.psh2196@gmail.com>
2020-05-27 11:05:09 +0000

    ユーザー一覧ページを作成中。タスク編集時バリデーションのエラ〜メッセージが出ない

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
   
-  validates :name, presence: true, length: { maximum: 30}
+  validates :name, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 300 }
 end
